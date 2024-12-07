a825d64d4b1f639b8a4391ee42d4f20155061ace
LuisHernandez1999 <luisohernandez27@gmail.com>
2024-12-07 02:15:19 -0300

    tabela  de  renda

--- main.py
+++ main.py
@@ -180,9 +180,61 @@ def grafico_pie():
     canva.draw()
 
 # chamar as funções
+
+
+## criando frame abaixo do medio 
+frame_renda=Frame(frameBaixo, width=300, height= 250, bg=co1)
+frame_renda.grid(row=0,column=0)
+
+
+frame_operacoes=Frame(frameBaixo, width=220, height= 250, bg=co1)
+frame_operacoes.grid(row=0,column=1,padx= 5)
+
+frame_configuracao=Frame(frameBaixo, width=220, height= 250, bg=co1)
+frame_configuracao.grid(row=0,column=2,padx= 5)
+
+### tabela renda mensal 
+app_tabela=Label(frameMeio, text="Tabelas Receitas e Despesas",anchor= NW, font=("Verdana 12"),bg=co1, fg=co4)
+app_tabela.place(x=5, y=309)
+
+def mostrar_renda():
+
+   
+    tabela_head = ['#Id','Categoria','Data','Quantia']
+
+    lista_itens = [[0,2,3,4],[0,2,3,4],[0,2,3,4],[0,2,3,4]]
+    
+    global tree
+
+    tree = ttk.Treeview(frame_renda, selectmode="extended",columns=tabela_head, show="headings")
+   
+    vsb = ttk.Scrollbar(frame_renda, orient="vertical", command=tree.yview)
+
+    hsb = ttk.Scrollbar(frame_renda, orient="horizontal", command=tree.xview)
+
+    tree.configure(yscrollcommand=vsb.set, xscrollcommand=hsb.set)
+
+    tree.grid(column=0, row=0, sticky='nsew')
+    vsb.grid(column=1, row=0, sticky='ns')
+    hsb.grid(column=0, row=1, sticky='ew')
+
+    hd=["center","center","center", "center"]
+    h=[30,100,100,100]
+    n=0
+
+    for col in tabela_head:
+        tree.heading(col, text=col.title(), anchor=CENTER)
+        tree.column(col, width=h[n],anchor=hd[n])
+        
+        n+=1
+
+    for item in lista_itens:
+        tree.insert('', 'end', values=item)
+
+
 grafico_bar()
 porcentagem()
+mostrar_renda()
 resumo()
 grafico_pie()
-
 janela.mainloop()
