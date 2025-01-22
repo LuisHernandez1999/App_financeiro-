09a5fa5f44151e35a2b0db23a1b39304c0fdd11b
LuisHernandez1999 <luisohernandez27@gmail.com>
2025-01-22 15:34:20 -0300

    mudanças no db

--- view.py
+++ view.py
@@ -2,7 +2,7 @@ import sqlite3 as lite
 
 con = lite.connect('dados.db')
 
-# Função para inserir categorias
+
 def inserir_categoria(i):
    with con:
       cur = con.cursor()
@@ -10,35 +10,35 @@ def inserir_categoria(i):
       cur.execute(query, (i,))
       print(f"Categoria '{i}' inserida com sucesso!")
 
-# Função para inserir receita
+
 def inserir_receita(i):
    with con:
       cur = con.cursor()
       query = "INSERT INTO Receitas (categoria, adicionando_em, valor) VALUES(?, ?, ?)"
       cur.execute(query, i)
 
-# Função para inserir gasto
+
 def inserir_gasto(i):
    with con:
       cur = con.cursor()
       query = "INSERT INTO Gastos (categoria, retirando_em, valor) VALUES(?, ?, ?)"
       cur.execute(query, i)
 
-# Função para deletar receita
+
 def deletar_receita(i):
    with con:
       cur = con.cursor()
       query = "DELETE FROM Receitas WHERE id=?"
-      cur.execute(query, (i,))  # Passando como tupla
+      cur.execute(query, (i,)) 
 
-# Função para deletar gasto
+
 def deletar_gasto(i):
    with con:
       cur = con.cursor()
       query = "DELETE FROM Gastos WHERE id=?"
-      cur.execute(query, (i,))  # Passando como tupla
+      cur.execute(query, (i,)) 
 
-# Função para ver categorias
+
 def ver_categoria():
     lista_itens = []
     with con:
@@ -49,7 +49,7 @@ def ver_categoria():
             lista_itens.append(l)
     return lista_itens
 
-# Função para ver gastos
+
 def ver_gastos():
     lista_itens = []
     with con:
@@ -60,7 +60,7 @@ def ver_gastos():
             lista_itens.append(l)
     return lista_itens
 
-# Função para ver receitas
+
 def ver_receitas():
     lista_itens = []
     with con:
@@ -81,12 +81,12 @@ def tabela():
     for i in receitas:
         tabela_lista.append(i)
     return tabela_lista
-# Função para calcular o total de valores de receitas e gastos (bar_valores)
+
 def bar_valores():
     total_receitas = 0
     total_gastos = 0
 
-    # Somar os valores das receitas
+   
     with con:
         cur = con.cursor()
         cur.execute("SELECT valor FROM Receitas")
@@ -94,7 +94,7 @@ def bar_valores():
         for receita in receitas:
             total_receitas += receita[0]
 
-    # Somar os valores dos gastos
+   
     with con:
         cur = con.cursor()
         cur.execute("SELECT valor FROM Gastos")
